40a8d114d3af61d187b6b06dda1cf179847ad2dd
mcamagna <camagna.maurizio@gmail.com>
2022-04-01 17:24:41 +0900

    Removed unused --no_sorting command

--- GFF2DDBJ.py
+++ GFF2DDBJ.py
@@ -31,7 +31,6 @@ def main():
     parser.add_argument('--strain', help="Name of the strain.")
     parser.add_argument('--mol_type', help="Type of molecule used in the sample. If not provided, you will be asked to choose the type if necessary.")
     parser.add_argument('--locus_tag_prefix', help="A prefix that is attached before each gene name. Must be 3-12 letters long and contain only alphanumeric characters. The first character should be a letter.")
-    parser.add_argument('--no_sorting', action='store_true', help="By default, the features of each source entry will be sorted by position. Use this flag to disable this behaviour.")
     parser.add_argument('--export_all', action='store_true', help="Parses the GFF completely, but only writes the source and CDS features. For genome annotations this is typically sufficient and can avoid difficulties such as alternatative splicing, which is not handled well in DDBJ files.")
     parser.add_argument('--gene_as_note', action='store_true', help="By default, the gene name/id will be written as 'gene' qualifier into each feature belonging to that gene. Using this flag, each feature will instead be labeled with 'note gene ID' instead.")
     parser.add_argument('--intermediate_gff', help="Optional: Output path for the intermediate GFF file. During parsing of the GFF files, some changes to the information in the GFF file may need to be introduced to allow exporting the file. Writing this intermediate GFF file can be useful to track down sources of error.")
@@ -47,7 +46,6 @@ def main():
     Parameters.source_attributes['mol_type'] = args.mol_type
     Parameters.source_attributes['strain'] = args.strain
     Parameters.locus_attributes["locus_tag_prefix"] = args.locus_tag_prefix
-    Parameters.sort_features = not args.no_sorting
     Parameters.export_all = args.export_all
     Parameters.gene_as_note = args.gene_as_note
     Parameters.intermediate_gff = args.intermediate_gff
